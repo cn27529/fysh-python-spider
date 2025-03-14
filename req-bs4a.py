d8a8b1d90c701c48e08f80c4f21cdcc4cd0085c5
cn27529 <cn27529@hotmail.com>
2025-03-14 13:55:08 +0800

    url update

--- req-bs4a.py
+++ req-bs4a.py
@@ -1,15 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-# url = "https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"  # 替換為您要抓取的網站 URL
-url = "https://books.toscrape.com/"
+# url = "https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
+url = "https://books.toscrape.com/"  # 替換為您要抓取的網站 URL
 response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     title = soup.title.string
     print(f"網頁標題：{title}")
-
     print("所有連結：")
     for link in soup.find_all("a"):
         href = link.get("href")
--- req.py
+++ req.py
@@ -1,11 +1,12 @@
 import requests
 
-response = requests.get("https://www.example.com")
-# response = requests.get("https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant")
+url = "https://www.example.com"
+# url = "https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
+response = requests.get(url)
 
 if response.status_code == 200:
-    # print("成功取得網頁內容")
+    print("成功取得網頁內容")
     print(response.text)
 else:
-    # print(f"無法取得網頁內容，狀態碼：{response.status_code}")
-    #print()
+    print(f"無法取得網頁內容，狀態碼：{response.status_code}")
+    # print()
