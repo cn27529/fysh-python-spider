024d99f01a4d0c5a22311d9e1752d5189fe88b3d
cn27529 <cn27529@hotmail.com>
2025-03-16 21:45:22 +0800

    scrapy_go

---
+++ scrapy_go/scrapy_go/spiders/book_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+from test_scrapy.test_scrapy.items import BookItem
+
+
+class BookSpider(scrapy.Spider):
+    name = "books"
+    start_urls = ["http://books.toscrape.com/"]
+
+    def parse(self, response):
+        for book in response.css("article.product_pod"):
+            item = BookItem()
+            item["title"] = book.css("h3 a::attr(title)").get()
+            item["author"] = book.css("div.product_price p.price_color::text").get()
+            yield item
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
