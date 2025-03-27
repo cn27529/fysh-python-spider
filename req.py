3874ca65a65c6496ef2945db84185211e6a9568a
cn27529 <cn27529@hotmail.com>
2025-03-27 20:46:03 +0800

    response.encoding

--- req.py
+++ req.py
@@ -3,6 +3,7 @@ import requests
 url = "https://www.example.com"
 # url = "https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
 response = requests.get(url)
+response.encoding='utf-8'
 
 if response.status_code == 200:
     print("成功取得網頁內容")
